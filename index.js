75f207b28d9d0b73966175d43cd832aee0714297
Keval Khawane <kevalkhawane9@gmail.com>
2023-11-13 20:48:41 +0530

    Initial commit

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ app.use((req, res) => {
 });
 
 const port = process.env.PORT || 3000; // Use the specified port or default to 3000
-app.listen(port, () => {
+app.listen(port,  () => {
   console.log(`Server is running on port ${port}`);
 });
